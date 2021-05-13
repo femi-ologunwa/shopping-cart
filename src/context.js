d3473f6e2bdc456683f6ad239be8a4cb566bf7d3
Femi <ologunwafemi@gmail.com>
2021-05-13 18:37:53 +0100

    added setup for the initial state and reducer

--- src/context.js
+++ src/context.js
@@ -1,17 +1,26 @@
-import React, { useState, createContext } from 'react';
+import React, { useReducer, createContext } from 'react';
 import cartItems from './data';
+import reducer from './reducer';
 
 //create a context
 const AppContext = createContext();
 
+//initial state
+const initialState = {
+   loading: false,
+   cart: cartItems,
+   total: 0,
+   amount: 0,
+};
+
 //create the context component/provider
 const AppProvider = ({ children }) => {
-   const [cart, setCart] = useState(cartItems);
+   const [state, dispatch] = useReducer(reducer, initialState);
 
    return (
       <AppContext.Provider
          value={{
-            cart,
+            ...state,
          }}
       >
          {children}
