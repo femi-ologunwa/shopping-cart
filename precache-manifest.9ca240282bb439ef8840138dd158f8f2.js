34f711955b030ce1141a0b192b9acd7aa3f68d4a
Femi <ologunwafemi@gmail.com>
2021-05-14 05:07:32 +0100

    Updates

---
+++ precache-manifest.9ca240282bb439ef8840138dd158f8f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e035bb171f59a01a5a38b9f3726318",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "ff7f01ce91de6c2a6dbc",
+    "url": "/shopping-cart/static/css/main.35134b96.chunk.css"
+  },
+  {
+    "revision": "3dbc2bc8418ac9033fb1",
+    "url": "/shopping-cart/static/js/2.3e4bfc4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shopping-cart/static/js/2.3e4bfc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7f01ce91de6c2a6dbc",
+    "url": "/shopping-cart/static/js/main.9bac2ad6.chunk.js"
+  },
+  {
+    "revision": "ba80342531975976b290",
+    "url": "/shopping-cart/static/js/runtime-main.6e52efc5.js"
+  }
+]);
